3a659ee2eb665de98307b575bba939821d1b33ca
bella <bella@LAPTOP-PQOTO1M5.(none)>
2021-11-16 17:32:18 -0800

    fetching data from api

--- utils/autocomplete.js
+++ utils/autocomplete.js
@@ -1,11 +1,33 @@
+// const CreateAutoComplete = { fetchData }
+// apikey: 17a1fc3fa922438dbbe01411211711
+// 'http://api.weatherapi.com/v1/current.json?key=17a1fc3fa922438dbbe01411211711&q=London&aqi=no'
+
+const fetchData = async (searchInput) => {
+  const response = await axios.get(
+    'http://api.weatherapi.com/v1/current.json?key=17a1fc3fa922438dbbe01411211711&q=London&aqi=no',
+    {
+      params: {
+        q: searchInput,
+      },
+    }
+  );
+
+  console.log(response);
+  console.log(response.data.location);
+};
+
 const form = document.querySelector('.autocomplete');
 form.innerHTML = `
   <form>
-    <label for='searchInput'></label>
+    <label for="searchInput"></label>
     <input
-      id='searchInput'
-      type='text'
+      id="input"
+      type="text"
       placeholder='Search for a location...'
     ></input>
+    <button type="submit">Submit</button>
   </form>
 `;
+
+const input = document.querySelector('#input');
+input.addEventListener('input', fetchData());
