f5eb4e9c356da930b7cd31f31619c4f10753ddc6
bella <bella@LAPTOP-PQOTO1M5.(none)>
2021-11-16 21:15:21 -0800

    simple misc things

--- utils/autocomplete.js
+++ utils/autocomplete.js
@@ -1,24 +1,17 @@
 // const CreateAutoComplete = { fetchData }
 // apikey: 17a1fc3fa922438dbbe01411211711
-// 'http://api.weatherapi.com/v1/current.json?key=17a1fc3fa922438dbbe01411211711&q=London&aqi=no'
 const fetchData = async (searchInput) => {
-  const response = await axios.get(
-    'http://api.weatherapi.com/v1/search.json',
-    {
-      params: {
-        key: '17a1fc3fa922438dbbe01411211711',
-        q: searchInput,
-
-        // appid: 'd18c77fc02324072392d419dc2f3b9d8',
-      },
+  const response = await axios.get('http://api.weatherapi.com/v1/search.json', {
+    params: {
+      key: '17a1fc3fa922438dbbe01411211711',
+      q: searchInput,
     },
-  );
-  console.log(response.data);
+  });
+  console.log(response);
   // console.log(response.data.location);
 };
 
 const form = document.querySelector('.autocomplete');
-
 
 form.innerHTML = `
   <form>
@@ -47,10 +40,8 @@ const onInput = async (evt) => {
   const data = await fetchData(evt.target.value);
   if (data) {
     const dropdown = document.querySelector('.dropdown-menu');
-    dropdown.removeAttribute('hidden');
+    //dropdown.removeAttribute('hidden');
   }
-
 };
-
 
 input.addEventListener('input', onInput);
